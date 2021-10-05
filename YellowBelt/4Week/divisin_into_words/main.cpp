6b9ba7a8c0cb5dddf2689665ac717e796a9f81b0
ZubarevP <zubarev.pol@gmail.com>
2021-10-05 23:35:18 +0200

    05.10.2021 home

---
+++ YellowBelt/4Week/divisin_into_words/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> SplitIntoWords(const string& s) {
+  vector<string> words;
+  auto start = s.begin();
+  auto finish = s.begin();
+  while(finish < s.end()) {
+    finish = find( start, s.end(), ' ');
+    words.push_back({start, finish});
+    ++finish;
+    start = finish;
+  }
+  return words;
+}
+
+int main() {
+  string s = "C Cpp Java Python";
+
+  vector<string> words = SplitIntoWords(s);
+  cout << words.size() << " ";
+  for (auto it = begin(words); it != end(words); ++it) {
+    if (it != begin(words)) {
+      cout << "/";
+    }
+    cout << *it;
+  }
+  cout << endl;
+  
+  return 0;
+}
