6d18557c36cad54a5bde4dff6b737215340fd178
ZubarevP <zubarev.pol@gmail.com>
2021-10-17 18:33:48 +0200

    17.10.2021 #2

---
+++ YellowBelt/5Week/DIY_inheritance/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+    Animal(const string& t) 
+    : Name(t) {
+    }
+
+    const string Name;
+};
+
+
+class Dog : public Animal {
+public:
+    Dog(const string& t) 
+    : Animal(t) {
+    }
+    void Bark() {
+        cout << Name << " barks: woof!" << endl;
+    }
+};
