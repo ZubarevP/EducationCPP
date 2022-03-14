f12dee42679cbdf6ff931161d33702467c66b5d3
Pavel <zubarev.pol@gmail.com>
2022-03-14 19:48:16 +0200

    14.03.2022 #2

--- BrownBelt/4_week/raii_booking/booking.h
+++ BrownBelt/4_week/raii_booking/booking.h
@@ -1,10 +1,39 @@
-#include <iostream>
+#pragma once
+#include <stdexcept>
+#include <utility>
 
 namespace RAII {
 
-template <typename Provider>
-class Booking {
-public:
-private:
-};
+    template <typename T>
+    class Booking {
+    public:
+        Booking() = default;
+        Booking(T* prov_, int count_) 
+            : prov (prov_)
+            , count (count_)
+        {
+        }
+
+        Booking(const Booking&) = delete;
+        Booking(Booking&& other) 
+            : prov(std::move(other.prov))
+            , count (other.count)
+        {
+            prov->counter++;
+        }
+
+        Booking& operator= (const Booking&) = delete;
+        Booking& operator= (Booking&& other) = default;
+
+        ~Booking() {
+            if(prov) {
+                prov->CancelOrComplete(*this);
+            }
+        }
+
+    private:
+        T* prov;
+        int count;
+    };
+
 }
