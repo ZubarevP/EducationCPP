b05b1ec54695489a14b5b69f35fe0af39e2fd3a6
Pavel <zubarev.pol@gmail.com>
2022-03-15 14:15:32 +0200

    15.03.2022 #3

--- BrownBelt/5_week/ebook/slow.cpp
+++ BrownBelt/5_week/ebook/slow.cpp
@@ -7,78 +7,49 @@ using namespace std;
 
 class ReadingManager {
 public:
-  ReadingManager()
-      : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-        sorted_users_(),
-        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+    ReadingManager()
+        : users(MAX_USER_COUNT_ + 1, -1)
+        , pages(MAX_BOOK_PAGE + 1, 0)
+    {}
 
-  void Read(int user_id, int page_count) {
-    if (user_page_counts_[user_id] == 0) {
-      AddUser(user_id);
+    void Read(int id, int page) {
+        if(users[id] < 0) {
+            counter++;
+            Flag(1, page);
+        } else {
+            Flag(users[id] + 1, page);
+        }
+        users[id] = page;
     }
-    user_page_counts_[user_id] = page_count;
-    int& position = user_positions_[user_id];
-    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-      SwapUsers(position, position - 1);
-    }
-  }
 
-  double Cheer(int user_id) const {
-    if (user_page_counts_[user_id] == 0) {
-      return 0;
+    double Cheer(int id) const {
+
+        const int last_page = users[id];
+        const int more = pages[last_page];
+
+        if(counter == 1 && more == 1) return 1;
+        if(counter == 0 || users[id] < 0 || more == counter) return 0;
+        return ((counter - more) / (counter * 1.0 - 1) );
     }
-    const int user_count = GetUserCount();
-    if (user_count == 1) {
-      return 1;
-    }
-    const int page_count = user_page_counts_[user_id];
-    int position = user_positions_[user_id];
-    while (position < user_count &&
-      user_page_counts_[sorted_users_[position]] == page_count) {
-      ++position;
-    }
-    if (position == user_count) {
-        return 0;
-    }
-    // По умолчанию деление целочисленное, поэтому
-    // нужно привести числитель к типу double.
-    // Простой способ сделать это — умножить его на 1.0.
-    return (user_count - position) * 1.0 / (user_count - 1);
-  }
 
 private:
-  // Статическое поле не принадлежит какому-то конкретному
-  // объекту класса. По сути это глобальная переменная,
-  // в данном случае константная.
-  // Будь она публичной, к ней можно было бы обратиться снаружи
-  // следующим образом: ReadingManager::MAX_USER_COUNT.
-  static const int MAX_USER_COUNT_ = 100'000;
+    static const int MAX_USER_COUNT_ = 100'000;
+    static const int MAX_BOOK_PAGE = 1'000;
 
-  vector<int> user_page_counts_;
-  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-  vector<int> user_positions_; // позиции в векторе sorted_users_
+    vector<int> users;
+    vector<int> pages;
+    
+    int counter = 0;
 
-  int GetUserCount() const {
-    return sorted_users_.size();
-  }
-  void AddUser(int user_id) {
-    sorted_users_.push_back(user_id);
-    user_positions_[user_id] = sorted_users_.size() - 1;
-  }
-  void SwapUsers(int lhs_position, int rhs_position) {
-    const int lhs_id = sorted_users_[lhs_position];
-    const int rhs_id = sorted_users_[rhs_position];
-    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-  }
+    void Flag(int start, int finish) {
+        for( int i = start; i <= finish; ++i) {
+            pages[i]++;
+        }
+    }
 };
 
 
 int main() {
-  // Для ускорения чтения данных отключается синхронизация
-  // cin и cout с stdio,
-  // а также выполняется отвязка cin от cout
-  ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
   ReadingManager manager;
