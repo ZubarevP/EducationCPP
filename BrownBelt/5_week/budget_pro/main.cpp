130f6f671a37145704d0eb2b93cb65be10885641
Pavel <zubarev.pol@gmail.com>
2022-03-24 17:54:41 +0200

    24.03.2022 #2

--- BrownBelt/5_week/budget_pro/main.cpp
+++ BrownBelt/5_week/budget_pro/main.cpp
@@ -101,7 +101,8 @@ istream& operator>> (istream& input, Range& range) {
 
 ////////////////////////////////////////////////////////////////////////
 struct Money {
-    double value = 0.0;
+    double earn = 0.0;
+    double spend = 0.0;
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -117,54 +118,64 @@ public:
 
     using Iterator = map<Date, Money>::iterator;
 
-    void ComputeIncome(istream& input = cin, ostream& output = cout) {
-        Range range;
-        input >> range;
+    Budget& Spend(Range range, istream& input = cin) {
+        double spend_money;
+        input >> spend_money;
+
+        spend_money = spend_money * ONE_DAY / difftime(range.second._time, range.first._time);
+
+        for(auto& period : MakeRange(range.first, range.second)) {
+            period.second.spend += spend_money;
+        }
+        return *this;
+    }
+
+    Budget& ComputeIncome(Range range, ostream& output = cout) {
         double result = 0.0;
 
-        Date second(range.second._time + ONE_DAY);
-        AddTimePoint(range.first);
-        AddTimePoint(second);
-
         auto start = storage.find(range.first);
-        auto finish = storage.find(second);
+        auto finish = storage.find(range.second);
 
         for( ; start != finish; start++) {
             auto dist = difftime(next(start)->first._time, start->first._time) / 
                         ONE_DAY;
-            result += start->second.value * dist;
+            result += start->second.earn * dist;
+            result -= start->second.spend * dist;
         }
 
         output << result << '\n';
+        return *this;
     }
 
-    void Earn(istream& input = cin){
-        Range range;
+    Budget& Earn(Range range, istream& input = cin){
         double value;
-        input >> range >> value;
+        input >> value;
 
-        Date second(range.second._time + ONE_DAY);
-        AddTimePoint(range.first);
-        AddTimePoint(second);
+        value = value * ONE_DAY / difftime(range.second._time, range.first._time);
 
-        value = value * ONE_DAY / difftime(second._time, range.first._time);
-
-        for(auto& period : MakeRange(range.first, second)) {
-            period.second.value += value;
+        for(auto& period : MakeRange(range.first, range.second)) {
+            period.second.earn += value;
         }
+        return *this;
     }
 
-    void PayTax(istream& input = cin) {
+    Budget& PayTax(Range range, istream& input = cin) {
+        int tax;
+        input >> tax;
+        
+        for(auto& period : MakeRange(range.first, range.second)) {
+            period.second.earn = period.second.earn * (100 - tax) / 100;
+        }
+        return *this;
+    }
+
+    Range MakePoints(istream& input = cin) {
         Range range;
         input >> range;
-        
-        Date second(range.second._time + ONE_DAY);
+        range.second._time += + ONE_DAY;
         AddTimePoint(range.first);
-        AddTimePoint(second);
-        
-        for(auto& period : MakeRange(range.first, second)) {
-            period.second.value *= 0.87;
-        }
+        AddTimePoint(range.second);
+        return range;
     }
 
     map<Date, Money>& Get() {
@@ -188,21 +199,17 @@ private:
         auto right = storage.find(rhs);
         return RBF<Iterator>(left, right);
     }
+
     
 };
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-//void TestDate();
-//void TestRange();
-//void TestBudget();
+
 
 int main () {
-    //TestRunner tr;
-    //RUN_TEST(tr, TestDate);
-    //RUN_TEST(tr, TestRange);
-    //RUN_TEST(tr, TestBudget);
+
     cout.precision(25);
     Budget budget;
     int num;
@@ -211,74 +218,20 @@ int main () {
     for(int i = 0; i < num; ++i) {
         string command;
         cin >> command;
+
         if(command == "ComputeIncome") {
-            budget.ComputeIncome();
+            budget.ComputeIncome(budget.MakePoints());
         }
         else if (command == "Earn") {
-            budget.Earn();
+            budget.Earn(budget.MakePoints());
         }
         else if (command == "PayTax") {
-            budget.PayTax();
+            budget.PayTax(budget.MakePoints());
+        } 
+        else if (command == "Spend") {
+            budget.Spend(budget.MakePoints());
         }
     }
     
     return 0;
 }
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-
-//void TestDate() {
-//    Date one(2022, 01, 31);
-//    Date two(2022, 02, 01);
-//    ASSERT(one < two);
-//    Date three;
-//    three.Add(2022, 02, 01);
-//    one._time += ONE_DAY;
-//    ASSERT(one == two);
-//    ASSERT(three == two);
-//
-//    istringstream input {"2022-02-01"};
-//    Date four;
-//    input >> four;
-//    ASSERT(two == four);
-//}
-//
-//void TestRange() {
-//    istringstream input {"2022-02-01 2022-02-03 2022-02-01 2022-02-03"};
-//    Range range;
-//    Date first, last;
-//    input >> range >> first >> last;
-//    ASSERT(range.first == first);
-//    ASSERT(range.second == last);
-//}
-//
-//void TestBudget() {
-//    ostringstream output;
-//    istringstream input {
-//        "2020-06-01 2020-06-30 300\n"
-//        "2020-06-30 2020-06-30 10\n"
-//        "2000-01-01 2099-12-31\n"
-//        "2020-06-29 2020-06-30\n"
-//        "2000-01-01 2099-12-31\n"
-//        "2000-01-01 2099-12-31\n"
-//        "2019-06-01 2019-06-30\n"
-//        "2019-06-01 2019-06-30\n"
-//        "2000-01-01 2099-12-31\n"
-//    };
-//    Budget budget;
-//    budget.Earn(input);
-//    budget.Earn(input);
-//    ASSERT_EQUAL(budget.Get()[Date(2020, 06, 01)].value, 10);
-//    ASSERT_EQUAL(budget.Get()[Date(2020, 06, 30)].value, 20);
-//    ASSERT_EQUAL(budget.Get().size(), 5);
-//
-//    budget.ComputeIncome(input, output);
-//    budget.ComputeIncome(input, output);
-//    budget.PayTax(input);
-//    budget.ComputeIncome(input, output);
-//    budget.PayTax(input);
-//    budget.ComputeIncome(input, output);
-//    budget.ComputeIncome(input, output);
-//    ASSERT_EQUAL(output.str(), "310\n30\n269.7\n0\n269.7\n");
-//}
